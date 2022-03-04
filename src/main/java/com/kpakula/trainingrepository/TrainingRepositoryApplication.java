4b94351534c4a5a9d6a91805614ff838c8c032f0
Konrad Pakula <konrad.a.pakula@gmail.com>
2022-03-04 16:02:11 +0100

    Create spring boot project and configure database connection and liquibase

---
+++ src/main/java/com/kpakula/trainingrepository/TrainingRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.kpakula.trainingrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingRepositoryApplication.class, args);
+    }
+
+}
